6c6443b63d3c5d9774eea549fefaa3be59af0a93
Pavel Papou <ppapou27@gmail.com>
2023-10-22 20:20:31 -0400

    Sport event application
     - base project structure initiated
     - build scripts are setup

---
+++ src/main/java/com/example/sport/SportEventApp.java
@@ -0,0 +1,13 @@
+package com.example.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportEventApp.class, args);
+	}
+
+}
